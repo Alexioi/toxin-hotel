8c4ad8c52380a8f645596569ead2f13096814563
Alexey Ivanov <alexioi1993@gmail.com>
2021-05-07 14:42:20 +0300

    переделал layout

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -2,13 +2,16 @@ import "./pagination.scss";
 
 import "paginationjs";
 
-$(".pagination").pagination({
-  dataSource: function (done) {
-    var result = [];
-    for (var i = 1; i < 180; i++) {
-      result.push(i);
-    }
-    done(result);
-  },
-  pageSize: 12,
-});
+if (document.querySelector(".pagination") !== null) {
+  $(".pagination").pagination({
+    dataSource: function (done) {
+      var result = [];
+      for (var i = 1; i < 180; i++) {
+        result.push(i);
+      }
+      done(result);
+    },
+    pageSize: 12,
+    pageRange: 1,
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./style/main.scss";
-import "./templates/templates.scss";
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
