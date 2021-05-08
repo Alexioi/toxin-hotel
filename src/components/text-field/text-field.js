ea60aab4581263ce11bb1210b2465735840fbab7
Alexey Ivanov <alexioi1993@gmail.com>
2021-05-08 19:05:54 +0300

    переписал text-field

---
+++ src/components/text-field/text-field.js
@@ -0,0 +1,40 @@
+import "./text-field.scss";
+
+import IMask from "imask";
+
+let maskOptions = {
+  mask: Date,
+  blocks: {
+    d: {
+      mask: IMask.MaskedRange,
+      placeholderChar: "Д",
+      from: 1,
+      to: 31,
+      maxLength: 2,
+    },
+    m: {
+      mask: IMask.MaskedRange,
+      placeholderChar: "М",
+      from: 1,
+      to: 12,
+      maxLength: 2,
+    },
+    Y: {
+      mask: IMask.MaskedRange,
+      placeholderChar: "Г",
+      from: 1900,
+      to: 9999,
+    },
+  },
+  overwrite: true,
+  autofix: true,
+  lazy: false,
+};
+
+let date = document.getElementsByClassName("js-text-field__input");
+
+if (date.length != 0) {
+  for (let i = 0; i < date.length; i++) {
+    IMask(date[i], maskOptions);
+  }
+}
