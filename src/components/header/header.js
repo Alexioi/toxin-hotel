a50933baa11c925d56902bb2ea48efb17b32246f
Alexey Ivanov <alexioi1993@gmail.com>
2021-05-25 12:05:01 +0300

    дорабатывал header, добавил адаптивности

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,1 +1,28 @@
 import "./header.scss";
+
+class Header {
+  constructor(component) {
+    this.component = component;
+    this.button = this.component.querySelector(".js-header__burger-button");
+    this.nav = this.component.querySelector(".js-header__nav");
+    this.login = this.component.querySelector(".js-header__login");
+    this._attachEventHandlers();
+  }
+
+  _attachEventHandlers() {
+    this.button.addEventListener("click", () => {
+      this._toggleNav();
+    });
+  }
+
+  _toggleNav() {
+    this.nav.classList.toggle("header__nav_opened");
+    this.login.classList.toggle("header__login_opened");
+  }
+}
+
+(() => {
+  document.querySelectorAll(".js-header").forEach((node) => {
+    new Header(node);
+  });
+})();
