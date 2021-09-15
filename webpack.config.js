d2ede095526312ab7df5a8ddd4317975949e2a20
Ivanov Alexey <ivanovdev@disroot.org>
2021-09-15 19:38:13 +0300

    разделил форму регистрации и авторизации на 2 формы

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
-const path = require("path");
-const fs = require("fs");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const webpack = require("webpack");
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
-const PAGES = fs.readdirSync(path.join(__dirname, "./src/pages"));
+const PAGES = fs.readdirSync(path.join(__dirname, './src/pages'));
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.pug$/i,
-        use: ["pug-loader"],
+        use: ['pug-loader'],
       },
       {
         test: /\.(svg|png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "./img/[contenthash].[ext]",
+              name: './img/[contenthash].[ext]',
             },
           },
         ],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
     }),
     new CleanWebpackPlugin(),
     ...PAGES.map(
@@ -52,11 +52,11 @@ module.exports = {
         new HtmlWebpackPlugin({
           filename: `${page}.html`,
           template: `./src/pages/${page}/${page}.pug`,
-        })
+        }),
     ),
   ],
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
   },
 };
