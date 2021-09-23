e146a604a316a118cee3b0908d07ef8ae010e5f6
Ivanov Alexey <ivanovdev@disroot.org>
2021-09-23 15:38:31 +0300

    рефакторинг dropdown menu

--- src/components/dropdown-menu/dropdown-menu.js
+++ src/components/dropdown-menu/dropdown-menu.js
@@ -3,22 +3,30 @@ import EventEmitter from 'event-emitter';
 class DropdownMenu {
   constructor($node) {
     this.$node = $node;
-    this.$toggleButton = this.$node.find('.dropdown-menu__input button');
-    this.$items = this.$node.find('.dropdown-menu__item');
-    this.$clearButton = this.$node.find('.dropdown-menu__clear');
-    this.$input = this.$node.find('.dropdown-menu__input input');
-    this.$applyButton = this.$node.find('.dropdown-menu__apply');
 
+    this._init();
+  }
+
+  _init() {
+    this._findNodes();
     this._initItems();
     this._attachEventHandlers();
     this._updateInput();
+  }
+
+  _findNodes() {
+    this.$input = this.$node.find('.js-dropdown-menu__input input');
+    this.$toggleButton = this.$node.find('.js-dropdown-menu__input button');
+    this.$items = this.$node.find('.js-dropdown-menu__item');
+    this.$clearButton = this.$node.find('.js-dropdown-menu__clear');
+    this.$applyButton = this.$node.find('.js-dropdown-menu__apply');
   }
 
   _initItems() {
     this.dropdownItems = [];
 
     this.$items.each((i, node) => {
-      this.dropdownItems[i] = new DropdownMenuCounter($(node));
+      this.dropdownItems[i] = new DropdownMenuItem($(node));
     });
   }
 
@@ -26,29 +34,14 @@ class DropdownMenu {
     this.$toggleButton.on('click', () => {
       this._toggleMenu();
     });
-
-    this.$clearButton.on('click', () => {
-      this._resetCounters();
-    });
-    this.$applyButton.on('click', () => {
-      this._updateInput();
-    });
-
-    this.dropdownItems[0].on('updatedCounter', () => {
-      this._updateInput();
-    });
-
-    this.dropdownItems[1].on('updatedCounter', () => {
-      this._updateInput();
-    });
-
-    this.dropdownItems[2].on('updatedCounter', () => {
-      this._updateInput();
-    });
   }
 
   _toggleMenu() {
     this.$node.toggleClass('dropdown-menu_opened');
+
+    const isOpened = this.$input[0].dataset.opened === 'true';
+
+    this.$input[0].dataset.opened = isOpened ? 'false' : 'true';
   }
 
   _resetCounters() {
@@ -67,13 +60,24 @@ class DropdownMenu {
     const value = this._calculateValue(counterValues);
 
     this.$input.val(value);
-    this._toggleMenu();
   }
 
   _calculateValue() {}
 }
 
 class DropdownMenuGuests extends DropdownMenu {
+  _attachEventHandlers() {
+    super._attachEventHandlers();
+
+    this.$clearButton.on('click', () => {
+      this._resetCounters();
+    });
+    this.$applyButton.on('click', () => {
+      this._updateInput();
+      this._toggleMenu();
+    });
+  }
+
   _calculateValue(counterValues) {
     const dropdownItems = [
       ['гость', 'гости'],
@@ -104,6 +108,16 @@ class DropdownMenuGuests extends DropdownMenu {
 }
 
 class DropdownMenuRooms extends DropdownMenu {
+  _attachEventHandlers() {
+    super._attachEventHandlers();
+
+    this.$items.each((i) => {
+      this.dropdownItems[i].on('updatedCounter', () => {
+        this._updateInput();
+      });
+    });
+  }
+
   _calculateValue(counterValues) {
     const dropdownItems = [
       ['спальня', 'спален'],
@@ -134,13 +148,31 @@ class DropdownMenuRooms extends DropdownMenu {
   }
 }
 
-class DropdownMenuCounter {
+class DropdownMenuItem {
   constructor($node) {
     this.$node = $node;
+
+    this._init();
+  }
+
+  resetCounter() {
+    this.$counter.text('0');
+    this.emit('updatedCounter');
+  }
+
+  getCounter() {
+    return this.$counter.text();
+  }
+
+  _init() {
+    this._findNodes();
+    this._attachEventHandlers();
+    this._disableButton();
+  }
+
+  _findNodes() {
     this.$counter = this.$node.find('.js-dropdown__counter');
     this.$counterButtons = this.$node.find('.js-dropdown__counter-button');
-
-    this._attachEventHandlers();
   }
 
   _attachEventHandlers() {
@@ -158,6 +190,7 @@ class DropdownMenuCounter {
     const newCounter = String(oldCounter + 1);
 
     this.$counter.text(newCounter);
+    this._enableButton();
     this.emit('updatedCounter');
   }
 
@@ -171,30 +204,33 @@ class DropdownMenuCounter {
     const newCounter = String(oldCounter - 1);
 
     this.$counter.text(newCounter);
+    this._disableButton();
     this.emit('updatedCounter');
   }
 
-  resetCounter() {
-    this.$counter.text('0');
-    this.emit('updatedCounter');
+  _disableButton() {
+    const counter = Number(this.$counter.text());
+
+    if (counter === 0) {
+      this.$counterButtons[0].classList.add('dropdown-menu__counter-button_disabled');
+    }
   }
 
-  getCounter() {
-    return this.$counter.text();
+  _enableButton() {
+    this.$counterButtons[0].classList.remove('dropdown-menu__counter-button_disabled');
   }
 }
 
-// EventEmitter(DropdownMenu.prototype);
-EventEmitter(DropdownMenuCounter.prototype);
+EventEmitter(DropdownMenuItem.prototype);
 
-(() => {
+$(() => {
   $('.js-dropdown-menu.dropdown-menu_type-guests').each((i, node) => {
     new DropdownMenuGuests($(node));
   });
-})();
+});
 
-(() => {
+$(() => {
   $('.js-dropdown-menu.dropdown-menu_type-rooms').each((i, node) => {
     new DropdownMenuRooms($(node));
   });
-})();
+});
