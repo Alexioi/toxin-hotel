cd60488e680f9188e94b9c7ac63ee064947b12bf
Ivanov Alexey <ivanovdev@disroot.org>
2021-09-24 16:03:31 +0300

    немного переделал структуру проекта

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import "./style/main.scss";
-import "./templates/ui-kit.scss";
+import './style/main.scss';
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
 }
 
-requireAll(require.context("./components", true, /\.js$/));
-requireAll(require.context("./components", true, /\.scss$/));
+requireAll(require.context('./components', true, /\.js$/));
+requireAll(require.context('./components', true, /\.scss$/));
 
-requireAll(require.context("./pages", true, /\.js$/));
-requireAll(require.context("./pages", true, /\.scss$/));
+requireAll(require.context('./pages', true, /\.js$/));
+requireAll(require.context('./pages', true, /\.scss$/));
+
+requireAll(require.context('./templates', true, /\.scss$/));
