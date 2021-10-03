368316ffab93913bfe4a8ce2b050ef315775ff28
Ivanov Alexey <ivanovdev@disroot.org>
2021-10-03 16:21:23 +0300

    рефакторинг LIKE BUTTON

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,32 +1,51 @@
 class LikeButton {
-  constructor(component) {
-    this.component = component;
-    this.content = this.component.querySelector('.like-button__content');
+  constructor($component) {
+    this.$component = $component;
+
+    this._init();
+  }
+
+  _init() {
+    this._findElements();
     this._attachEventHandlers();
+    this._changeButtonIcon();
+  }
+
+  _findElements() {
+    this.$icon = this.$component.find('.like-button__icon');
+    this.$counter = this.$component.find('.like-button__counter');
   }
 
   _attachEventHandlers() {
-    this.component.addEventListener('click', () => {
+    this.$component.on('click', () => {
       this._changeButtonStyle();
+      this._changeButtonIcon();
       this._setCounterLikes();
     });
   }
 
   _changeButtonStyle() {
-    this.component.classList.toggle('like-button_liked');
+    this.$component.toggleClass('like-button_liked');
+  }
+
+  _changeButtonIcon() {
+    const isLiked = this.$component.hasClass('like-button_liked');
+    const icon = isLiked ? 'favorite' : 'favorite_border';
+
+    this.$icon.text(icon);
   }
 
   _setCounterLikes() {
-    if (this.component.classList.contains('like-button_liked')) {
-      this.content.innerHTML++;
-    } else {
-      this.content.innerHTML--;
-    }
+    const currentCounter = Number(this.$counter.text());
+    const isLiked = this.$component.hasClass('like-button_liked');
+    const newCounter = isLiked ? currentCounter + 1 : currentCounter - 1;
+
+    this.$counter.text(newCounter);
   }
 }
 
-(() => {
-  document.querySelectorAll('.js-like-button').forEach((node) => {
-    new LikeButton(node);
+$(() => {
+  $('.js-like-button').each((i, node) => {
+    new LikeButton($(node));
   });
-})();
+});
