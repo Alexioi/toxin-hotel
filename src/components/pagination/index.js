e84e8f4662427064b87810c8aa71a4f316fdf88b
Ivanov Alexey <ivanovdev@disroot.org>
2022-10-14 10:44:37 +0300

    move plugin style to libs

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -1,5 +1,6 @@
 import Pagination from './Pagination';
 import cssSelectors from './constants';
+import '../../libs/paginationjs/paginationjs.scss';
 
 $(cssSelectors.pagination).each((i, node) => {
   new Pagination($(node));
--- src/components/range-slider/index.js
+++ src/components/range-slider/index.js
@@ -1,5 +1,6 @@
 import cssSelectors from './constants';
 import RangeSlider from './RangeSlider';
+import '../../libs/ion-rangeslider/ion-rangeslider.scss';
 
 $(cssSelectors.slider).each((i, node) => {
   new RangeSlider($(node));
