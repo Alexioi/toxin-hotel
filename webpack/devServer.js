0145a7e0864fc6891c9485eee6daa5125d53381c
Ivanov Alexey <ivanovdev@disroot.org>
2022-12-11 10:51:12 +0300

    add dev server option publicPath = "toxin-hotel"

--- webpack/devServer.js
+++ webpack/devServer.js
@@ -2,6 +2,7 @@ module.exports = (path) => {
   return {
     devServer: {
       contentBase: path,
+      publicPath: '/toxin-hotel',
       compress: true,
       port: 3000,
       host: '0.0.0.0',
