b0d364e0ccac50fe3558337a179a3b0849dae129
Ivanov Alexey <ivanovdev@disroot.org>
2023-07-09 17:13:26 +0300

    create Callback type in event emmiter #5

--- src/helpers/EventEmitter/index.ts
+++ src/helpers/EventEmitter/index.ts
@@ -1,11 +1,13 @@
-class EventEmitter<T> {
+type Callback<T extends object, K extends keyof T> = (value: T[K]) => void;
+
+class EventEmitter<T extends object> {
   private events: {
-    [eventName in keyof T]?: ((value: T[eventName]) => void)[];
+    [eventName in keyof T]?: Callback<T, eventName>[];
   } = {};
 
   public subscribe<K extends keyof T>(
     eventName: K,
-    callback: (value: T[K]) => void,
+    callback: Callback<T, K>,
   ): void {
     const events = this.events[eventName];
 
@@ -20,7 +22,7 @@ class EventEmitter<T> {
 
   public unsubscribe<K extends keyof T>(
     eventName: K,
-    callback: (value: T[K]) => void,
+    callback: Callback<T, K>,
   ): void {
     this.events[eventName] = this.events[eventName]?.filter((eventCallback) => {
       return callback !== eventCallback;
